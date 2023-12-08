b18d3e48f718c26eb4e861d66cf3d4c0b15ccc25
VincentDS98 <disanto.vincenzo98@gmail.com>
2023-12-08 03:34:00 +0100

    Completo

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+for (let i = 0; i<=100; i++){
+    console.log(i);
+
+     if((i % 3 == 0)&& (i % 5 == 0)){
+        console.log('FizzBuzz',i);
+    }
+
+    else if(i % 3 == 0){
+        console.log('Fizz',i);
+    }
+
+    else if(i % 5 == 0){
+        console.log('Buzz',i);
+    }
+
+    
+
+    else{
+        console.log(1);
+    }
+
+}
